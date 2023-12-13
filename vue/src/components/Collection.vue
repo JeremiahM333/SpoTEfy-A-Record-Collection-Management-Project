90a6f40c47a3a4a9546762b9954c3c2ffd9618d5
Jeremy Meyers <jeremymeyers1301@gmail.com>
2023-12-13 16:14:01 -0500

    Changed styling of collection card

--- vue/src/components/Collection.vue
+++ vue/src/components/Collection.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="collection-card">
         <router-link :to="{ name: 'records', params: { collectionId: collection.collectionId } }"
-            @click="setCurrentCollectionId(collection.collectionId), changeLocation('Collection Details')" class="router-link-custom">
+            @click="setCurrentCollectionId(collection.collectionId), changeLocation('Collection Details')"
+            class="router-link-custom">
             <div class="card text-center">
                 <img :src="collectionCover" class="card-img-top" id="card-picture" @error="replaceWithDefault()">
                 <div class="card-body" id="title-body">
                     <h4 class="card-text">{{ collection.collectionName }}</h4>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted"> </small>
                 </div>
             </div>
         </router-link>
@@ -51,32 +55,43 @@ export default {
     width: 16%;
     opacity: 90%;
     border-radius: 1.5rem;
-    
+
 
 }
 
-.card-text {
-    text-align: center;
-    text-decoration: none;
-    text-transform: none;
+.card-body {
     min-height: 6rem;
     max-height: 6rem;
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-text {
+    /* text-align: center; */
+    text-decoration: none;
+    text-transform: none;
 }
 
 #card-picture {
     max-height: 15rem;
     min-height: 15rem;
-    
+
 }
 
 #title-body {
     align-items: center;
+    border-top: solid 1px silver;
+    border-bottom: solid 1px silver;
 }
 
 .router-link-custom {
     text-decoration: none;
     color: inherit;
+}
+
+.card-footer {
+    border-top: none;
 }
 </style>
   
